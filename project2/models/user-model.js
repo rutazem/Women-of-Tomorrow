ef9ac4d6fa30593fa567cf01d2dbb65cce996039
Ruta Zemcugovaite <rutzem@gmail.com>
2020-04-25 17:38:28 +0200

    first-repo

---
+++ project2/models/user-model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+ 
+const userSchema = new Schema(
+  {
+    username: String,
+    password: String,
+
+    ///// need more clarity with this
+    role: {
+      type: String,
+      enum : ['mentor','mentee']
+
+    }
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+  }
+);
+ 
+const User = mongoose.model('User', userSchema);
+ 
+module.exports = User;
